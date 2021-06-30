6fe224a943d32c762a706881101f915867c1a5db
Ken Faubel <ken@faubel.org>
2021-06-30 12:01:24 -0400

    FIx module export in index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // This is used when embedding this in a larger solution
 // Use app.ts for local testing and to build a stand alone solution
 
-const NewsData  = require('./build/NewsData');
-const NewsImage = require('./build/NewsImage');
+// This did not work
+// const NewsData  = require('./build/NewsData');
+// const NewsImage = require('./build/NewsImage');
+// module.exports = { NewsData, NewsImage };
 
-module.exports = { NewsData, NewsImage };
+// This does
+module.exports = Object.assign({}, require('./build/NewsData'), require('./build/NewsImage'));
